33f41f4322a2aaa90cdf17347de00940011cc3f3
sergiorvs2 <sergio.valdivia@dekoding.pe>
2021-04-20 19:30:02 -0500

    added complaints book

--- core/schema/query.py
+++ core/schema/query.py
@@ -14,7 +14,12 @@ class Query(ObjectType):
     Graphql Queries
     """
     privacy_politics = String()
+    complaints_book = String()
 
     @staticmethod
     def resolve_privacy_politics(self, info):
         return staticfiles_storage.url("documents/privacity_politics.pdf")
+
+    @staticmethod
+    def resolve_complaints_book(self, info):
+        return staticfiles_storage.url("documents/complaints_book.docx")
